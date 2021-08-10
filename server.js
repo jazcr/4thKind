6b0f1ab062b10058c67de6a15e7a54dcf12b7a46
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-08-09 18:48:07 -0600

    server and connection file

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
-//Required packages
-//const path = require('path');
-const express= require('express');
+const express = require('express');
 const routes = require('./controllers');
-const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const path = require('path');
 
-//Create app object and the PORT
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3304;
 
-const hbs = exphbs.create({});
+const SequelizeStore = require('express-session-sequelize')(session.Store);
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+//add sessions
+const sess = {
+    secret: 'cookiemonster',
+    cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
 
-//Set up express to grab data
+app.use(session(sess));
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//naming handlebars as our engine
+app.engine('handlebars', hbs.engine);
+//setting handlebars as our view engine
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-//sync sequelize model to database and then turn on and listen to server
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+    app.listen(PORT, () => console.log('Now listening on http://localhost:3304'));
 });
